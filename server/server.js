fd7ba87996a715887e38aa40995f558812756931
ShijazKs <hafizshijaz@gmail.com>
2023-12-26 15:42:15 +0530

    server update

--- server/server.js
+++ server/server.js
@@ -19,24 +19,24 @@ const app = express();
 //middleware
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["https://minimo-six.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: ["https://minimo-six.vercel.app"],
+//     methods: ["POST", "GET"],
+//     credentials: true,
+//   })
+// );
 
 app.get("/", (req, res) => {
-  res.json("Hello");
-})
+  res.send("Hello");
+});
 
 //routes
 app.use("/users", require("./routes/userRoute"));
 
 //transaction
 app.use("/transaction", require("./routes/transactionRoute"));
-
 
 //port
 const PORT = 8080 || process.env.PORT;
